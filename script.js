ef1a6d4bfd315d84167f4ade33788b587104d083
Szymon Harajda <szymonharajda@gmail.com>
2023-11-18 23:12:26 +0100

    to-do-app

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var del = document.getElementsByClassName("delete");
+var lis = document.getElementsByTagName("li");
 
+liElements();
+buttonDelete()
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
 	var li = document.createElement("li");
+	var button =document.createElement("button")
 	li.appendChild(document.createTextNode(input.value));
+	button.appendChild(document.createTextNode("delete"));
+	li.appendChild(button);
 	ul.appendChild(li);
 	input.value = "";
+
+	liElements();
+	buttonDelete();
+
 }
 
 function addListAfterClick() {
@@ -25,6 +36,28 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function liElements(){
+	for(let i=0; i<lis.length;i++){
+		lis[i].addEventListener("click", addClass);
+	}
+}
+
+function addClass(){
+	this.classList.toggle("done");
+}
+function buttonDelete(){
+	var btn = document.querySelectorAll("li button");
+	for(let i =0; i<btn.length;i++){
+		btn[i].addEventListener("click", function(e){
+				e.target.parentElement.remove();			
+		})
+	}
+}
+
+
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+
